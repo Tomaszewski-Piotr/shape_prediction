a633181a71069e310a02762bcd65b1c64fe1bb8f
Tomaszewski-Piotr <71523553+Tomaszewski-Piotr@users.noreply.github.com>
2021-03-22 16:44:00 +0100

    Add xgb classifiers
    *Extends classifier set with xg classifiers
    *Expands command line capabilities

--- train.py
+++ train.py
@@ -37,15 +37,49 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 import argparse
 from datetime import datetime
+from xgboost import XGBClassifier
+from xgboost import XGBRFClassifier
+from xgboost import XGBRFClassifier
+import math
+from sklearn.base import is_classifier
+import matplotlib.pyplot as plt
+from sklearn.metrics import plot_confusion_matrix
+from PIL import Image
+from yellowbrick.classifier import ConfusionMatrix
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+import shutil
+from joblib import dump, load
 
 #start time measurement
 start_time = datetime.now()
 
-
 #parse input
+predefined_switches = [
+    ('--default', '-d', 'default_classifier', 'Run RandomForest and XGBClassifier'),
+    ('--limited', '-l', 'limited_classifier', 'Run RandomForest, XGBClassifier, KNeighbors, NeuralNet '),
+    ('--full', '-f', 'all_classifier', 'Run all classifiers (painfully slow and rather pointless)')]
+
+algorithm_switches = [
+    ('--rfc',  'rfc_classifier', 'Run RandomForestClassifier'),
+    ('--xgb',  'xgb_classifier', 'Run XGBClassifier'),
+    ('--xrfc', 'xrfc_classifier', 'Run XGBRFClassifier'),
+    ('--mlp',  'mlp_classifier', 'Run MLPClassifier'),
+    ('--dct',  'dct_classifier', 'Run DecisionTreeClassifier'),
+    ('--ada',  'ada_classifier', 'Run AdaBoostClassifier'),
+    ('--gnb',  'gnb_classifier', 'Run GaussianNB'),
+    ('--qda',  'qda_classifier', 'Run QuadraticDiscriminantAnalysis'),
+    ('--svc',  'svc_classifier', 'Run SVC'),
+    ('--lr',   'lr_classifier', 'Run LogisticRegression'),
+    ('--knc',  'knc_classifier', 'Run KNeighborsClassifier'),
+    ('--nn',   'nn_classifier', 'Run NeuralNet (do not run stacked)')
+]
+
 parser = argparse.ArgumentParser(description='Demo for various training shape recognition methods')
 parser.add_argument('--pca', '-p', action='store', dest='pca',
                     help='Perform PCA on input data. Values below 1 for explained variability, from 1 number of PCA components')
+parser.add_argument('--extended', action='store_true', default=False, dest='extended',
+                    help='Produces predictions for the complete data set')
 parser.add_argument('--verbose', '-v', action='store_true', default=False,
                     dest='verbose',
                     help='Set verbose mode')
@@ -53,10 +87,18 @@ parser.add_argument('--upload', '-u', action='store_true', default=False,
                     dest='upload',
                     help='Upload results to Neptune, NEPTUNE_API_TOKEN must be set in the shell')
 group = parser.add_mutually_exclusive_group()
-group.add_argument('--default', '-d',  action='store_true', dest='default_classifier', help='Run RandomForest (default choice)')
-group.add_argument('--limited', '-l', action='store_true', dest='limited_classifier', help='Run RandomForest, KNeighbors, NeuralNet and Stacked (slow)')
-group.add_argument('--full', '-f', action='store_true', dest='all_classifier',help='Run all classifiers (painfully slow and rather pointless)')
+for item in predefined_switches:
+    group.add_argument(item[0], item[1], action='store_true', dest=item[2], help=item[3])
+
+for item in algorithm_switches:
+        parser.add_argument(item[0], action='store_true', dest=item[1], help=item[2])
+
+#add option to run stacked
+parser.add_argument('--stacked', '-s', action='store_true', default=False,
+                    dest='stc_classifier',
+                    help='Additionally run selected classifiers stacked')
 results = parser.parse_args()
+
 
 def log_verbose(*args):
     if results.verbose:
@@ -66,15 +108,29 @@ def log_verbose(*args):
 # Just disables the warning, doesn't enable AVX/FMA
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+#prepare output directory
+output_dir = 'output'
+log_verbose('\nResults will be placed in: ', output_dir)
+if os.path.isdir(output_dir):
+    log_verbose('\nCleaning output directory')
+    shutil.rmtree(output_dir)
+os.mkdir(output_dir);
+
+
+def output_file(filename):
+    return pathlib.Path(output_dir, filename)
+
 # load the datasets
 data_path = 'data'
 data_suffix = '_Iq.csv'
 
 # concat contents off all data files
+log_verbose('\nPreparing data')
+
 content = []
 for txt_file in pathlib.Path(data_path).glob('*' + data_suffix):
     filename = os.path.basename(txt_file)
-    log_verbose('Retrieving data from: ' + filename)
+    log_verbose(' Retrieving data from: ' + filename)
     # read in data using pandas
     shape = pd.read_csv(txt_file)
     #add 'shape' column with shape name
@@ -97,13 +153,14 @@ all_y = all_df[['shape']]
 # encode class values as integers
 encoder = LabelEncoder()
 encoder.fit(all_y.values.ravel())
-encoded_Y = encoder.transform(all_y.values.ravel())
-# convert integers to dummy variables (i.e. one hot encoded)
-one_hot_y = np_utils.to_categorical(encoded_Y)
+# get ordinal encoding
+ordinal_y = encoder.transform(all_y.values.ravel())
+# get one hot encoding
+one_hot_y = np_utils.to_categorical(ordinal_y)
 
 
 #split into test and train
-X_train, X_test_base, y_train, y_test, one_hot_y_train, one_hot_y_test = train_test_split(all_X, all_y, one_hot_y,  test_size=0.2) # 80% training and 20% test
+X_train, X_test_base, y_train, y_test, one_hot_y_train, one_hot_y_test, ordinal_y_train, ordinal_y_test = train_test_split(all_X, all_y, one_hot_y, ordinal_y, test_size=0.2) # 80% training and 20% test
 
 #normalize input
 scaler = StandardScaler()
@@ -120,16 +177,14 @@ if results.pca:
         pca_parameter = int(pca_parameter);
     pca = PCA(pca_parameter)
     pca.fit(X_train)
-    log_verbose("PCA reduction to:", pca.n_components_, "components, ", "{0:.0%}".format(pca.explained_variance_ratio_.sum()), 'of variance')
+    log_verbose("\nPCA reduction:")
+    log_verbose(" Number of selected components: ", pca.n_components_)
+    log_verbose(" Explained variance: {0:.0%}".format(pca.explained_variance_ratio_.sum()))
     # apply the PCA transform
     X_train = pca.transform(X_train)
     X_test = pca.transform(X_test)
-    all_X = pca.transform(all_X)
-#print(pca.n_samples_)
-#print(pca.components_)
-#print(pca.explained_variance_)
-#print(pca.explained_variance_ratio_)
-#print(len(pca.explained_variance_ratio_))
+    if results.extended:
+        all_X = pca.transform(all_X)
 
 
 #Prepare classifiers
@@ -173,81 +228,112 @@ def baseline_model2(train_cols, no_categories):
     return bm
 
 
-# compile the keras classifier. Arbitrarily select smaller classifier if data reduction (PCA) applied
-if results.pca:
-    keras_estimator = KerasClassifier(build_fn=baseline_model(train_cols,no_categories), epochs=500, batch_size=5, verbose=0)
-else:
-    keras_estimator = KerasClassifier(build_fn=baseline_model2(train_cols,no_categories), epochs=500, batch_size=5, verbose=0)
+log_verbose('\nPreparing classifiers')
+classifiers = []
 
+##default classifiers
+if results.all_classifier or results.limited_classifier or results.default_classifier or results.rfc_classifier:
+    classifiers.append(('RandomForestClassifier', RandomForestClassifier(**parameters)))
 
-#print('Evaluating keras')
-#keras_estimator.fit(X_train, one_hot_y_train)
-#keras_estimator.fit(all_X, one_hot_y)
-#y_pred = keras_estimator.predict(X_test)
-#y_pred_proba = keras_estimator.predict_proba(X_test)
-#print(y_pred_proba.shape)
-#print(y_pred_proba)
+if results.all_classifier or results.limited_classifier or results.default_classifier or results.xgb_classifier:
+    classifiers.append(('XGBClassifier', XGBClassifier(use_label_encoder=False, eval_metric = "merror")))
 
+#limited classifiers
 
-#accuracy = metrics.accuracy_score(y_test, encoder.inverse_transform(y_pred))
-#print("Accu :", "{0:.0%}\n".format(accuracy))
-#exit(1)
-
-classifiers = [('RandomForestClassifier', RandomForestClassifier(**parameters))]
-
-if results.all_classifier:
-    classifiers.append(('MLPClassifier', MLPClassifier(alpha=1, max_iter=5000)))
-    classifiers.append(('DecisionTreeClassifier', DecisionTreeClassifier(max_depth=7)))
-    classifiers.append(('AdaBoostClassifier', AdaBoostClassifier()))
-    classifiers.append(('GaussianNB', GaussianNB()))
-    classifiers.append(('QuadraticDiscriminantAnalysis', QuadraticDiscriminantAnalysis()))
-    classifiers.append(('SVC', SVC(gamma='auto')))
-    classifiers.append(('LogisticRegression', LogisticRegression(max_iter = 5000)))
-
-if  results.all_classifier or results.limited_classifier:
+if results.all_classifier or results.limited_classifier or results.knc_classifier:
     classifiers.append(('KNeighborsClassifier', KNeighborsClassifier(3)))
-    classifiers.append(('Stacked', StackingClassifier(estimators=classifiers[:-1], final_estimator=LogisticRegression(max_iter=5000))))
+
+if results.all_classifier or results.limited_classifier or results.nn_classifier:
+    # compile the keras classifier. Arbitrarily select smaller classifier if data reduction (PCA) applied
+    if results.pca:
+        keras_estimator = KerasClassifier(build_fn=baseline_model(train_cols, no_categories), epochs=500, batch_size=5,
+                                          verbose=0)
+    else:
+        keras_estimator = KerasClassifier(build_fn=baseline_model2(train_cols, no_categories), epochs=500, batch_size=5,
+                                          verbose=0)
     classifiers.append(('NeuralNet', keras_estimator))
 
+
+#full classifiers
+if results.all_classifier or results.xrfc_classifier:
+    classifiers.append(('XGBRFClassifier', XGBRFClassifier(use_label_encoder=False, eval_metric = "merror", n_estimators=220)))
+
+if results.all_classifier or results.mlp_classifier:
+    classifiers.append(('MLPClassifier', MLPClassifier(alpha=1, max_iter=5000)))
+
+if results.all_classifier or results.dct_classifier:
+    classifiers.append(('DecisionTreeClassifier', DecisionTreeClassifier(max_depth=7)))
+
+if results.all_classifier or results.ada_classifier:
+    classifiers.append(('AdaBoostClassifier', AdaBoostClassifier()))
+
+if results.all_classifier or results.gnb_classifier:
+    classifiers.append(('GaussianNB', GaussianNB()))
+
+if results.all_classifier or results.qda_classifier:
+    classifiers.append(('QuadraticDiscriminantAnalysis', QuadraticDiscriminantAnalysis()))
+
+if results.all_classifier or results.svc_classifier:
+    classifiers.append(('SVC', SVC(gamma='auto')))
+
+if results.all_classifier or results.lr_classifier:
+    classifiers.append(('LogisticRegression', LogisticRegression(max_iter = 5000)))
+
+if results.stc_classifier:
+    classifiers.append(('Stacked', StackingClassifier(estimators=classifiers[:-1], final_estimator=LogisticRegression(max_iter=5000))))
+
 for name, clf in classifiers:
-    log_verbose('Added classifier: ', name)
+    log_verbose(' Added classifier: ', name)
 
 #list of classifiers where one hot encoding is required
 one_hot_encoded = ['NeuralNet']
 
-y_pred = {}
 accuracy = {}
 
 for name, clf in classifiers:
-    log_verbose("Evaluating: ", name)
+    log_verbose("\nEvaluating: ", name)
     train_start_time = datetime.now()
+    fig, ax = plt.subplots()
     if name in one_hot_encoded:
+
         clf.fit(X_train, one_hot_y_train)
-        train_end_time = datetime.now()
-        y_pred[name] = encoder.inverse_transform(clf.predict(X_test))
-        accuracy[name] = metrics.accuracy_score(y_test, y_pred[name])
-        all_df[name] = encoder.inverse_transform(clf.predict(all_X))
+        y_pred = encoder.inverse_transform(clf.predict(X_test))
+        accuracy[name] = metrics.accuracy_score(y_test, y_pred)
+        c_m = confusion_matrix(y_test, y_pred, labels = encoder.classes_ )
+        df_cfm = pd.DataFrame(c_m, index=encoder.classes_ , columns=encoder.classes_ )
+        plt.figure(figsize=(10, 7))
+        cfm_plot = sn.heatmap(df_cfm, annot=True, fmt='d')
+        plt.savefig(output_file(name + ".png"))
+        plt.close(fig)
     else:
-        clf.fit(X_train, y_train.values.ravel())
-        train_end_time = datetime.now()
-        y_pred[name] = clf.predict(X_test)
-        accuracy[name] = metrics.accuracy_score(y_test, y_pred[name])
-        all_df[name] = clf.predict(all_X)
-    print(name, "accuracy:", "{0:.0%}\n".format(accuracy[name]))
+        cm = ConfusionMatrix(clf, encoder=encoder, is_fitted=False, ax=ax)
+        cm.fit(X_train, ordinal_y_train)
+        accuracy[name] = cm.score(X_test, ordinal_y_test)
+        cm.finalize()
+        plt.savefig(output_file(name + ".png"))
+        plt.close(fig)
+
+    dump(clf, output_file(name + ".joblib"))
+    train_end_time = datetime.now()
+    #y_pred[name] = encoder.inverse_transform(clf.predict(X_test))
+    #accuracy[name] = metrics.accuracy_score(y_test, y_pred[name])
+    if results.extended:
+        all_df[name] = encoder.inverse_transform(clf.predict(all_X))
+    print('', name, "accuracy:", "{0:.0%}".format(accuracy[name]))
     eval_end_time = datetime.now()
-    log_verbose('Training time: {}'.format(train_end_time - train_start_time))
-    log_verbose('Evaluation time: {}'.format(eval_end_time - train_end_time))
+    log_verbose(' Training time: {}'.format(train_end_time - train_start_time))
+    log_verbose(' Evaluation time: {}\n'.format(eval_end_time - train_end_time))
 
-
-for i in X_test_base.index:
-    all_df.loc[i, 'test_set'] = "yes"
-
-#save and zip the results file
-log_verbose('Saving predictions')
-all_df.to_csv("predictions.csv")
+# save the results file if required
+if results.extended:
+    for i in X_test_base.index:
+        all_df.loc[i, 'test_set'] = "yes"
+    #save the results file
+    log_verbose('Saving predictions')
+    all_df.to_csv(output_file("predictions.csv"))
 
 end_time = datetime.now()
-log_verbose('Execution duration: {}'.format(end_time - start_time))
+log_verbose('Total execution time: {}'.format(end_time - start_time))
 
 if results.upload:
     if os.getenv('CI') == "true":
@@ -262,18 +348,37 @@ if results.upload:
             print('NEPTUNE_API_TOKEN must be specified in the shell')
             exit(1)
 
+
+    def zip_it(in_file, out_file):
+        inpath = output_file(in_file)
+        outpath = output_file(out_file)
+        with zipfile.ZipFile(outpath, "w", compression=zipfile.ZIP_DEFLATED) as zf:
+            zf.write(inpath, os.path.basename(inpath))
+        #unix style required by naptune
+        return outpath.as_posix()
+
     neptune.create_experiment(name='shape_prediction')
     for name, clf in classifiers:
+        log_verbose('Uploading data for: ', name)
         neptune.log_metric(name, accuracy[name])
-        if os.getenv('CI') == "true":
-            log_confusion_matrix_chart(clf, X_train, X_test, y_train, y_test)  # log confusion matrix chart
-            log_precision_recall_chart(clf, X_test, y_test)
-    # zip results
-    log_verbose("Zipping and uploading prediction file")
-    inpath = "predictions.csv"
-    outpath = "predictions.zip"
-    with zipfile.ZipFile(outpath, "w", compression=zipfile.ZIP_DEFLATED) as zf:
-        zf.write(inpath, os.path.basename(inpath))
-    neptune.log_artifact('predictions.zip')
+        # Load image
+        image = Image.open(output_file(name+".png"))
+        neptune.log_image('Confusion matrices', image, image_name=name, description='Confusion matrix for '+name)
+
+        #if os.getenv('CI') == "true" and is_classifier(clf):
+        #    if name in one_hot_encoded:
+        #        log_confusion_matrix_chart(clf, X_train, X_test, one_hot_y_train, one_hot_y_test)  # log confusion matrix chart
+        #        log_precision_recall_chart(clf, X_test, y_test)
+        #    else:
+        #        log_confusion_matrix_chart(clf, X_train, X_test, ordinal_y_train, ordinal_y_test)  # log confusion matrix chart
+        #        log_precision_recall_chart(clf, X_test, y_test)
+        neptune.log_artifact(zip_it(name + ".joblib", name + ".zip"))
+
+
+    # if requested zip and add extended results
+    if results.extended:
+        log_verbose("Zipping and uploading complete prediction result file")
+        neptune.log_artifact(zip_it("predictions.csv", "predictions.zip"))
+
     neptune.stop()
 
